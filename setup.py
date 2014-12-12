80b2ae0b0d331d22ab2e3b190341f20170d29187
Benjamin Kendinibilir <bkendinibilir@seibert-media.net>
2014-12-12 15:13:29 +0100

    add dependecies, bump version

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from distutils.core import setup
 
 setup(
     name='pylupusec',
-    version='0.1',
+    version='0.2',
     py_modules=['pylupusec'],
     description='Python Lupusec API',
     author='//SEIBERT/MEDIA GmbH',
     author_email='info@seibert-media.net',
     url='http://www.seibert-media.net',
+    requires=['requests'],
 )
